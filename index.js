faee316dfad8610c69686b9da61646c3a21d3746
Tobias Urban <tourban@microsoft.com>
2020-08-23 11:05:55 +0200

    Changed workflow file to js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = __importDefault(require("@actions/core"));
-var github_1 = __importDefault(require("@actions/github"));
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 try {
     // `who-to-greet` input defined in action metadata file
-    var name_1 = core_1.default.getInput('applicationName');
-    var isSecretRequired = core_1.default.getInput('requireSecret');
+    const name = core.getInput('applicationName');
+    const isSecretRequired = core.getInput('requireSecret');
+
+
     // Get the JSON webhook payload for the event that triggered the workflow
-    var payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
-    console.log("The event payload: " + payload);
-    core_1.default.setOutput("clientId", "SampleId");
-    core_1.default.setOutput("clientSecret", "");
-}
-catch (error) {
-    console.error(error);
-    core_1.default.setFailed(error.message);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    core.setOutput("clientId", "SampleId");
+    core.setOutput("clientSecret", "");
+} catch (error) {
+    core.setFailed(error.message);
 }
