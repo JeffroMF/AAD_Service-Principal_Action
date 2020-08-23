844f51760d4b2eec5ba493bb5e5b2c9e7062f13f
Tobias Urban <tourban@microsoft.com>
2020-08-23 12:00:24 +0200

    Typed logic

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fetch = require("node-fetch");
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __importDefault(require("@actions/core"));
+var node_fetch_1 = __importDefault(require("node-fetch"));
 try {
     // `who-to-greet` input defined in action metadata file
-    const adminAppId = core.getInput("adminApplicationId");
-    const adminAppSecret = core.getInput("adminApplicationSecret");
-    const tenantId = core.getInput("tenantId");
-    const name = core.getInput('applicationName');
-    const isSecretRequired = core.getInput('requireSecret');
-    const debugMode = core.getInput('requireSecret');
-
-    getToken(adminAppId, adminAppSecret, tenantId).then(token => {
-        createApplication(token, name).then(appId => {
-            core.setOutput("clientId", appId);
-            if (isSecretRequired) {
-                createSecret.then(secret => { core.setOutput("clientSecret", secret); })
+    var adminAppId = core_1.default.getInput("adminApplicationId");
+    var adminAppSecret = core_1.default.getInput("adminApplicationSecret");
+    var tenantId = core_1.default.getInput("tenantId");
+    var name_1 = core_1.default.getInput('applicationName');
+    var isSecretRequired_1 = core_1.default.getInput('requireSecret');
+    var debugMode_1 = core_1.default.getInput('requireSecret');
+    getToken(adminAppId, adminAppSecret, tenantId).then(function (token) {
+        createApplication(token, name_1).then(function (appId) {
+            core_1.default.setOutput("clientId", appId);
+            if (isSecretRequired_1) {
+                createSecret(token, appId).then(function (secret) {
+                    core_1.default.setOutput("clientSecret", secret);
+                    if (debugMode_1) {
+                        console.info("Client ID: " + appId);
+                        console.info("Client Secret: " + secret);
+                    }
+                });
             }
-            else core.setOutput("clientSecret", "");
-            if(debugMode) {
-                console.info("Client ID: " + appId);
-                console.info("Client Secret: " + secret);
+            else {
+                core_1.default.setOutput("clientSecret", "");
+                if (debugMode_1) {
+                    console.info("Client ID: " + appId);
+                    console.info("Client Secret: ");
+                }
             }
-        })
-    })
-} catch (error) {
-    core.setFailed(error.message);
+        });
+    });
 }
-
+catch (error) {
+    core_1.default.setFailed(error.message);
+}
 function getToken(appId, appSecret, tenantId) {
-    return new Promise(resolve => {
-        const queryParams = new URLSearchParams();
+    return new Promise(function (resolve) {
+        var queryParams = new URLSearchParams();
         queryParams.append('client_id', appId);
         queryParams.append('client_secret', appSecret);
         queryParams.append('scope', "Application.ReadWrite.All");
         queryParams.append('grant_type', "client_credentials");
-        const token = fetch("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token", {
+        var token = node_fetch_1.default("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token", {
             method: "POST",
             body: queryParams
-        }).then(res => {
-            res.json().then(json => {
+        }).then(function (res) {
+            res.json().then(function (json) {
                 resolve(json.access_token);
-            })
-        })
-    })
+            });
+        });
+    });
 }
 function createApplication(token, name) {
-    return new Promise(resolve => {
-        fetch("https://graph.microsoft.com/v1.0/applications", {
+    return new Promise(function (resolve) {
+        node_fetch_1.default("https://graph.microsoft.com/v1.0/applications", {
             method: "POST",
             headers: {
                 "Authorization": "Bearer " + token
@@ -55,16 +65,16 @@ function createApplication(token, name) {
             body: {
                 "displayName": name
             }
-        }).then(resp => {
-            resp.json().then(app => {
-                resolve(app.appId)
-            })
-        })
-    })
+        }).then(function (resp) {
+            resp.json().then(function (app) {
+                resolve(app.appId);
+            });
+        });
+    });
 }
 function createSecret(token, appId) {
-    return new Promise(resolve => {
-        fetch("https://graph.microsoft.com/v1.0/applications/" + appId + "/addPassword", {
+    return new Promise(function (resolve) {
+        node_fetch_1.default("https://graph.microsoft.com/v1.0/applications/" + appId + "/addPassword", {
             method: "POST",
             headers: {
                 "Authorization": "Bearer " + token
@@ -72,10 +82,10 @@ function createSecret(token, appId) {
             body: {
                 "displayName": "default"
             }
-        }).then(resp => {
-            resp.json().then(secret => {
+        }).then(function (resp) {
+            resp.json().then(function (secret) {
                 resolve(secret.secretText);
-            })
-        })
-    })
+            });
+        });
+    });
 }
---
+++ index.ts
@@ -0,0 +1,90 @@
+import core from '@actions/core';
+import nodeFetch from "node-fetch";
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const adminAppId = core.getInput("adminApplicationId");
+    const adminAppSecret = core.getInput("adminApplicationSecret");
+    const tenantId = core.getInput("tenantId");
+    const name = core.getInput('applicationName');
+    const isSecretRequired = core.getInput('requireSecret');
+    const debugMode = core.getInput('requireSecret');
+
+    getToken(adminAppId, adminAppSecret, tenantId).then(token => {
+        createApplication(token, name).then(appId => {
+            core.setOutput("clientId", appId);
+            if (isSecretRequired) {
+                createSecret(token, appId).then(secret => { 
+                    core.setOutput("clientSecret", secret);
+                    if(debugMode) {
+                        console.info("Client ID: " + appId);
+                        console.info("Client Secret: " + secret);
+                    }
+                })
+                
+            }
+            else {
+                core.setOutput("clientSecret", "");
+                if(debugMode) {
+                    console.info("Client ID: " + appId);
+                    console.info("Client Secret: ");
+                }
+            }
+            
+        })
+    })
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+function getToken(appId:string, appSecret:string, tenantId:string):Promise<string> {
+    return new Promise(resolve => {
+        const queryParams = new URLSearchParams();
+        queryParams.append('client_id', appId);
+        queryParams.append('client_secret', appSecret);
+        queryParams.append('scope', "Application.ReadWrite.All");
+        queryParams.append('grant_type', "client_credentials");
+        const token = nodeFetch("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token", {
+            method: "POST",
+            body: queryParams
+        }).then(res => {
+            res.json().then(json => {
+                resolve(json.access_token);
+            })
+        })
+    })
+}
+function createApplication(token:string, name:string):Promise<string> {
+    return new Promise(resolve => {
+        nodeFetch("https://graph.microsoft.com/v1.0/applications", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            body: {
+                "displayName": name
+            }
+        }).then(resp => {
+            resp.json().then(app => {
+                resolve(app.appId)
+            })
+        })
+    })
+}
+function createSecret(token:string, appId:string):Promise<string> {
+    return new Promise(resolve => {
+        nodeFetch("https://graph.microsoft.com/v1.0/applications/" + appId + "/addPassword", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            body: {
+                "displayName": "default"
+            }
+        }).then(resp => {
+            resp.json().then(secret => {
+                resolve(secret.secretText);
+            })
+        })
+    })
+}
